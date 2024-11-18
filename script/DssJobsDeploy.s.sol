a40a78b5c80ade8d663f89be787bb840047f2019
lrc-o <oliver@lrcltd.co>
2024-11-18 10:56:55 +0800

    Update DssJobsDeploy.sol; add safety checks, logging and job parameters config

--- script/DssJobsDeploy.s.sol
+++ script/DssJobsDeploy.s.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity <=0.8.13;
 
 import "forge-std/Script.sol";
@@ -5,115 +6,117 @@ import "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
 import {stdJson} from "forge-std/StdJson.sol";
 
-
-// Sequencer
+// Sequencer and Jobs imports
 import { Sequencer } from "dss-cron/Sequencer.sol";
-
-// Jobs
-import { AutoLineJob } from "dss-cron/AutoLineJob.sol";
-import { ClipperMomJob } from "dss-cron/ClipperMomJob.sol";
-import { D3MJob } from "dss-cron/D3MJob.sol";
-import { FlapJob } from "dss-cron/FlapJob.sol";
-import { LerpJob } from "dss-cron/LerpJob.sol";
-import { LiquidatorJob } from "dss-cron/LiquidatorJob.sol";
-import { LitePsmJob } from "dss-cron/LitePsmJob.sol";
-import { NetworkPaymentAdapter } from "dss-cron/NetworkPaymentAdapter.sol";
 import { OracleJob } from "dss-cron/OracleJob.sol";
-import { Sequencer } from "dss-cron/Sequencer.sol";
-import { VestedRewardsDistributionJob } from "dss-cron/VestedRewardsDistributionJob.sol";
+import { AutoLineJob } from "dss-cron/AutoLineJob.sol";
+import { FlapJob } from "dss-cron/FlapJob.sol";
 import { IJob } from "dss-cron/interfaces/IJob.sol";
-
 
 interface IlkRegistryLike {
     function list() external view returns (bytes32[] memory);
     function pip(bytes32 ilk) external view returns (address);
 }
 
-interface IChainLogLike {
-    function getAddress(bytes32 _key) external view returns (address addr);
+interface SpotterLike {
+    function ilks(bytes32) external view returns (address, uint256);
 }
 
-struct DssDeployArtifacts {
-  address authority;
-  address autoline;
-  address cat;
-  address clog;
-  address cure;
-  address dai;
-  address daiJoin;
-  address dog;
-  address dssDeploy;
-  address end;
-  address esm;
-  address feedCOL3;
-  address flap;
-  address flop;
-  address ilkRegistry;
-  address jug;
-  address pipCOL3;
-  address pipETH;
-  address pipUSDT;
-  address pipXINF;
-  address pot;
-  address psm;
-  address spotter;
-  address vat;
-  address vow;
+// Separate storage contract for artifacts
+contract DssAddresses {
+    address public authority;
+    address public autoline;
+    address public spotter;
+    address public ilkRegistry;
+    address public flap;
+    address public vow;
+    
+    constructor(
+        address _spotter,
+        address _ilkRegistry
+    ) {
+        spotter = _spotter;
+        ilkRegistry = _ilkRegistry;
+    }
 }
 
 contract DssJobsDeployScript is Script, Test {
     using stdJson for string;
 
-    // --- Math ---
-    uint256 constant WAD = 10 ** 18;
-    uint256 constant RAY = 10 ** 27;
-    uint256 constant RAD = 10 ** 45;
-
-    // Governance Parameters
-    uint256 constant INITIAL_XINF_SUPPLY = 1000000 * WAD;
-    uint256 constant INITIAL_USDT_SUPPLY = 10000000 * (10 ** 6); // USDT has 6 decimals
-
-    // Network
     bytes32 constant NET_MAIN = "NTWK-MAIN";
+    uint256 constant NETWORK_WINDOW = 10;
     
-    bytes32[] private toPoke;
-    bytes32[] private spotterIlksToPoke;
+    DssAddresses public addresses;
+    Sequencer public sequencer;
 
-    DssDeployArtifacts artifacts;
-    Sequencer sequencer;
-
-    function chainId() internal view returns (uint256 _chainId) {
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            _chainId := chainid()
-        }
-    }
-    function run() public {
-        vm.startBroadcast();
+    function loadArtifacts() internal {
         string memory root = vm.projectRoot();
-
         string memory path = string(
-            abi.encodePacked(root, "/script/output/", vm.toString(chainId()), "/dssDeploy.artifacts.json")
+            abi.encodePacked(
+                root, 
+                "/script/output/", 
+                vm.toString(block.chainid), 
+                "/dssDeploy.artifacts.json"
+            )
         );
         string memory json = vm.readFile(path);
-        bytes memory data = vm.parseJson(json);
-        artifacts = abi.decode(data, (DssDeployArtifacts));
+        
+        // Parse individual addresses from JSON
+        address spotter = json.readAddress(".spotter");
+        address ilkRegistry = json.readAddress(".ilkRegistry");
+        
+        addresses = new DssAddresses(
+            spotter,
+            ilkRegistry
+        );
+    }
 
+    function deploySequencer() internal {
         sequencer = new Sequencer();
-        sequencer.addNetwork(NET_MAIN, 10);
+        sequencer.addNetwork(NET_MAIN, NETWORK_WINDOW);
+        console.log("Sequencer deployed at: %s", address(sequencer));
+    }
 
-        bytes32[] memory v = IlkRegistryLike(artifacts.ilkRegistry).list();
-
-        OracleJob oracleJob = new OracleJob(address(sequencer), artifacts.ilkRegistry, artifacts.spotter);
+    function configureOracleJob() internal {
+        require(address(sequencer) != address(0), "Sequencer not deployed");
+        require(addresses.ilkRegistry() != address(0), "IlkRegistry not configured");
+        require(addresses.spotter() != address(0), "Spotter not configured");
         
+        OracleJob oracleJob = new OracleJob(
+            address(sequencer),
+            addresses.ilkRegistry(),
+            addresses.spotter()
+        );
+
         sequencer.addJob(address(oracleJob));
+        console.log("OracleJob deployed at: %s", address(oracleJob));
+    }
+
+    function verifyJobs() internal {
+        Sequencer.WorkableJob[] memory jobs = sequencer.getNextJobs(NET_MAIN);
+        console.log("Verifying %d jobs", jobs.length);
         
-        // Sequencer.WorkableJob[] memory jobs = sequencer.getNextJobs(NET_MAIN);
-        // for (uint256 i = 0; i < jobs.length; i++) {
-        //     if (jobs[i].canWork) {
-        //         IJob(jobs[i].job).work(NET_MAIN, jobs[i].args);
-        //     }
-        // }
+        for (uint256 i = 0; i < jobs.length; i++) {
+            console.log("Job %d: %s (workable: %s)", 
+                i,
+                address(jobs[i].job),
+                jobs[i].canWork ? "true" : "false"
+            );
+        }
+    }
+
+    function run() public {
+        vm.startBroadcast();
+
+        // Load artifacts and deploy sequencer
+        loadArtifacts();
+        deploySequencer();
+        
+        // Configure jobs
+        configureOracleJob();
+        
+        // Verify deployment
+        verifyJobs();
 
         vm.stopBroadcast();
     }
