c20eb26c50e07c85124f404737adb6eddcc2a424
lrc-z <lrc-z@pht>
2024-11-20 16:23:32 +0700

    :🌙 create artifact for solidity v0.8.0 libs

---
+++ test/Version8Build.t.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "dss-proxy/DssProxyRegistry.sol";
+
+contract Version8BuildTest is Test {
+    function testVersion8Build() public {
+        console.log("Hello, world!");
+        DssProxyRegistry registry = new DssProxyRegistry();
+        console.log("registry", address(registry));
+    }
+}
